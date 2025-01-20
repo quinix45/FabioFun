3511d9d056c2dee4e2841dd0b6e23460ae517acd
Fabio Setti <fabio.setti45@gmail.com>
2025-01-20 09:07:01 -0500

    Initial commit

---
+++ R/nice_3D_plot.R
@@ -0,0 +1,185 @@
+
+nice_3D_plot <- function(y, 
+                         x1, 
+                         x2, 
+                         axis_names = NULL, 
+                         dot_labels = NULL, 
+                         groups = NULL,  
+                         reg_plane = FALSE,
+                         interaction = FALSE, 
+                         plane_res = 50, ...){
+  
+  require(plotly, quietly = TRUE)
+  require(tidyverse, quietly = TRUE)
+  
+  plot_dat <- data.frame(y, x1, x2)
+  
+  
+  if(!is.null(axis_names)){
+    
+    axz <- list(title = axis_names[1])
+    axx <- list(title = axis_names[2])
+    axy <- list(title = axis_names[3])
+    
+  }else{
+    axz <- list(title = "Y")
+    axx <- list(title = "X1")
+    axy <- list(title = "X2")
+  }
+  
+  
+  if(is.null(dot_labels)){
+    
+    dot_labels <- paste("Row", 1:nrow(plot_dat))
+  }
+  
+  
+  p <- plot_ly(plot_dat,
+               z = ~ plot_dat[,1],
+               x = ~ plot_dat[,2],
+               y = ~ plot_dat[,3],
+               text = dot_labels,
+               hoverinfo = "text",
+               color = ~groups,
+               hovertemplate = paste0("%{text} <br>" ,
+                                      axx[[1]], ": %{x:0.} <br>",
+                                      axy[[1]], ": %{y:0.} <br>",
+                                      axz[[1]], ": %{z:0.}"),
+               type = "scatter3d",
+               mode = 'markers',
+               showlegend = !is.null(groups)
+               
+  ) %>%
+    layout(scene = list(xaxis= axx,yaxis= axy,zaxis= axz))
+  
+  if(isFALSE(reg_plane))
+  {
+    
+    # plot without regression plane
+    return(p)
+    
+  }
+  else if(isTRUE(reg_plane) & isFALSE(interaction))
+  {
+    # add regression plane
+    
+    
+    # regression model
+    
+    lm_mod <- lm(y ~ x1 + x2, data = plot_dat)
+    
+    # Values to calculate grid over
+    X1_grid <- seq(min(x1), max(x1), by = (max(x1) - min(x1))/plane_res)
+    X2_grid <- seq(min(x2), max(x2), by = (max(x2) - min(x2))/plane_res)
+    
+    
+    lm_surface <- expand.grid(x1 = X1_grid, x2 = X2_grid)
+    lm_surface$y <- predict.lm(lm_mod, newdata = lm_surface)
+    
+    require(reshape2, quietly = TRUE)
+    lm_surface <- acast(lm_surface, x1 ~ x2, value.var = "y") 
+    
+    
+    p_reg <- add_trace(p = p,
+                       z = round(lm_surface, 3),
+                       x = round(X1_grid, 3),
+                       y = round(X2_grid, 3),
+                       hovertemplate = paste0(axx[[1]], ": %{x:0.} <br>",
+                                              axy[[1]], ": %{y:0.} <br>",
+                                              axz[[1]], ": %{z:0.}"),
+                       type = "surface",
+                       colorscale = "Viridis",
+                       inherit = FALSE,
+                       showlegend = FALSE,
+                       name = paste("Predicted", axz[[1]]),
+                       colorbar = list(title = paste(axz[[1]]),
+                                       len = 0.5)) 
+    # plot with regression plane  
+    return(p_reg)
+  }
+  else if(isTRUE(reg_plane) & isTRUE(interaction))
+  {
+    # add regression plane
+    
+    
+    # regression model
+    
+    lm_mod <- lm(y ~ x1*x2 , data = plot_dat)
+    
+    # Values to calculate grid over
+    X1_grid <- seq(min(x1), max(x1), by = (max(x1) - min(x1))/plane_res)
+    X2_grid <- seq(min(x2), max(x2), by = (max(x2) - min(x2))/plane_res)
+    
+    
+    lm_surface <- expand.grid(x1 = X1_grid, x2 = X2_grid)
+    lm_surface$y <- predict.lm(lm_mod, newdata = lm_surface)
+    
+    require(reshape2, quietly = TRUE)
+    lm_surface <- acast(lm_surface, x1 ~ x2, value.var = "y") 
+    
+    
+    p_reg_int <- add_trace(p = p,
+                           z = round(lm_surface, 3),
+                           x = round(X1_grid, 3),
+                           y = round(X2_grid, 3),
+                           hovertemplate = paste0(axx[[1]], ": %{x:0.} <br>",
+                                                  axy[[1]], ": %{y:0.} <br>",
+                                                  axz[[1]], ": %{z:0.}"),
+                           type = "surface",
+                           colorscale = "Viridis",
+                           inherit = FALSE,
+                           showlegend = FALSE,
+                           name = paste("Predicted", axz[[1]]),
+                           colorbar = list(title = paste(axz[[1]]),
+                                           len = 0.5)) 
+    # plot with regression plane  
+    return(p_reg_int)
+  }
+  
+}
+
+
+
+
+### Example
+
+
+## Without regression Plane
+
+# plot_iris <- nice_3D_plot(y = iris$Sepal.Length,
+#                           x1 = iris$Petal.Length,
+#                           x2 = iris$Petal.Width,
+#                           dot_labels = paste("row", 1:150),
+#                           groups = iris$Species,
+#                           axis_names = c("Sepal Length", "PL", "PW"),
+#                           reg_plane = FALSE)
+# 
+# plot_iris
+# 
+
+
+## With regression plane
+
+
+# plot_iris_reg <- nice_3D_plot(y = iris$Sepal.Length,
+#                               x1 = iris$Petal.Length,
+#                               x2 = iris$Petal.Width,
+#                               groups = iris$Species,
+#                               axis_names = c("SL", "PL", "PW"),
+#                               reg_plane = TRUE)
+# 
+# plot_iris_reg
+
+
+## With interaction
+# 
+# plot_iris_int <- nice_3D_plot(y = iris$Sepal.Length,
+#                               x1 = iris$Petal.Length,
+#                               x2 = iris$Petal.Width,
+#                               groups = iris$Species,
+#                               axis_names = c("SL", "PL", "PW"),
+#                               reg_plane = TRUE,
+#                               interaction = TRUE)
+# 
+# plot_iris_int
+# 
---
+++ R/regression_flextable.R
@@ -0,0 +1,109 @@
+regression_flextable <- function(lm_object, title = "", var_names = NULL, intercept = TRUE)
+{
+  
+  require(tidyverse, quietly = TRUE)
+  require(flextable, quietly = TRUE)
+  
+  
+  lm_summary <- summary(lm_object)
+  
+  if(isTRUE(intercept))
+  {
+    lm_CI <- round(confint(lm_object),2)
+    lm_summary$coefficients <- lm_summary$coefficients[,-c(2)] 
+  } 
+  else 
+  {
+    lm_CI <- round(confint(lm_object)[-1,],2)
+    lm_summary$coefficients <- lm_summary$coefficients[-1,-c(2)] 
+  }
+  
+  
+  # p value for R^2
+  
+  Rsq_p <- round(pf(lm_summary$fstatistic[1], 
+                    lm_summary$fstatistic[2], 
+                    lm_summary$fstatistic[3], 
+                    lower.tail = FALSE),3)
+  
+  
+  if(Rsq_p < .05 & Rsq_p >.01){
+    
+    Rsq_p <- paste0(Rsq_p, "*")
+    
+  } else if(Rsq_p < .01 & Rsq_p >.001)
+  {
+    
+    Rsq_p <- paste0(Rsq_p, "**")
+    
+  } else if(Rsq_p <= .001)
+  {
+    Rsq_p <- ".001^{***}"
+  }
+  
+  
+  
+  lm_tab <- data.frame(cbind(row.names(lm_summary$coefficients), 
+                             round(lm_summary$coefficients[,1:2], 2),
+                             round(lm_summary$coefficients[,3], 3),
+                             paste0("[",lm_CI[,1],"; ",
+                                    lm_CI[,2], "]"),
+                             c(rep(NA, nrow(lm_CI)-1), 
+                               paste0("R^2=", 
+                                      round(lm_summary$r.squared, 2), 
+                                      ",F(", 
+                                      lm_summary$fstatistic[2], 
+                                      ",",
+                                      lm_summary$fstatistic[3],
+                                      ")=", 
+                                      round(lm_summary$fstatistic[1], 2),
+                                      ",p<", Rsq_p
+                               ))))
+  
+  colnames(lm_tab) <- c("Variable", "B", "t", "p", "95%CI", "R^2")
+  
+  
+  lm_tab$p <- as.numeric(ifelse(lm_tab$p == "0", .001, lm_tab$p))
+  
+  lm_tab$p[lm_tab$p < .05 & lm_tab$p >.01] <- paste0(lm_tab$p[lm_tab$p < .05 & lm_tab$p >.01], "^{*}")
+  lm_tab$p[lm_tab$p < .01 & lm_tab$p >.001] <- paste0(lm_tab$p[lm_tab$p < .01 & lm_tab$p >.001], "^{**}")
+  lm_tab$p[lm_tab$p <= .001] <- "<.001***"
+  
+  
+  
+  # change variable names if supplied
+  
+  if(is.null(var_names) == FALSE)
+  {
+    lm_tab$Variable <- var_names
+  }
+  
+  # remove NA
+  
+  lm_tab[is.na(lm_tab)] <- ""  
+  
+  
+  ft <- flextable(lm_tab)
+  ft <- ft %>%
+    flextable::compose(j = "R^2", value = as_paragraph(as_equation(lm_tab[,6]))) %>%
+    flextable::compose(j = "R^2", part = "header" , value = as_paragraph(as_equation("R^2"))) %>%
+    width( j = 6, 3.3, unit = "in") %>% 
+    bold(bold = TRUE, part = "header") %>%
+    line_spacing(space = 1.5, part = "all") %>%
+    set_caption(caption = title) %>%
+    font(fontname = "Times New Roman", part = "all") %>%
+    autofit(part = "body") %>% 
+    theme_apa()
+  
+  return(ft)
+}
+
+
+
+## Test
+
+# lm_mod <- lm(Sepal.Length ~ Petal.Length + Petal.Width, iris)
+# 
+# reg_table <- regression_flextable(lm_mod)
+# 
+# save_as_docx(reg_table, path = "table.docx")
