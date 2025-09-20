d98694ae1284e1d1608a0ee9076fc01d7ad06586
Fabio Setti <fabio.setti45@gmail.com>
2025-09-20 19:29:12 -0400

    Updated reactable info function with icon

--- R/reactbl_col.desc.R
+++ R/reactbl_col.desc.R
@@ -1,26 +1,36 @@
 
 reactbl_col.desc <- function(names, 
                             desc, 
-                            style = "text-decoration: underline; text-decoration-style: solid;"){
+                            style = "text-decoration: underline; text-decoration-style: solid;",
+                            info_icon = TRUE,
+                            info_color = "#007bff"){
 
 library(reactable)
-library(htmltools)
+  library(htmltools)
   
   list_res <- list()
   
-  for(i in 1:length(names)){
-
-   # turn into character and then html
-    list_res[[i]] <- reactable::colDef(header  = 
-     htmltools::tags$abbr(style = style,
-                title = desc[i],
-                names[i]))
-  } 
-
-  names(list_res) <- names
+  for (i in seq_along(names)) {
+    
+    if (info_icon) {
+      # Column name + info icon
+      header_content <- div(
+        style = "display: flex; align-items: center; gap: 4px;",
+        title = desc[i],
+        names[i],
+        span( "\u2139",  # info symbol
+              title = desc[i],
+              style = paste("cursor: pointer; color:", info_color, ";")))
+    } else {
+      # no info icon 
+      header_content <- tags$abbr(style = style, title = desc[i], names[i])
+    }
+    
+    list_res[[i]] <- reactable::colDef(header = header_content)
+  }
   
+  names(list_res) <- names
   return(list_res)
-
 }
 
 ######## Example ########
@@ -41,7 +51,8 @@ library(htmltools)
 
 
 # col_labs <- reactbl_col.desc(names = cols,
-#                              desc = desc_vec)
+#                              desc = desc_vec,
+#                              info_color = "red")
 
 
 # library(tidyverse)
