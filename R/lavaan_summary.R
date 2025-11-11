2f9e1f7312c6d59c81dc0c5e6c402ce1576ecf1b
Fabio Setti <fabio.setti45@gmail.com>
2025-11-11 11:17:29 -0500

    added lavaan summary function

---
+++ R/lavaan_summary.R
@@ -0,0 +1,13 @@
+
+# a function that returns a neater output for lavaan path models
+
+lavaan_summary <- function(lavaan_mod){
+  
+  require(tidyverse, quietly = TRUE)
+  
+  tab_sum <- lavaan::parameterestimates(lavaan_mod) |> 
+    dplyr::filter(op %in% c("~", ":=")) |> 
+    dplyr::select(any_of(c("lhs", "op", "rhs", "label", "est", "ci.lower", "ci.upper")))
+  
+  return(tab_sum)
+}
